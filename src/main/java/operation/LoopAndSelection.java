930de8b1cef1a00a72c42747364481507a22e166
HUONGPC\HUONGPC <huong.nguyen.intern@ntq-solution.com.vn>
2017-03-20 14:22:15 +0700

    update loopandselection

--- src/main/java/operation/LoopAndSelection.java
+++ src/main/java/operation/LoopAndSelection.java
@@ -10,28 +10,41 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class LoopAndSelection {
-	
+
 	static Scanner sc = new Scanner(System.in);
 
 	public static void main(String args[]) {
-		
+
 		System.out.println("nhap chuoi ");
 		String s = sc.nextLine();
 		String s1, s2;
 		String s3 = "";
 		int count = 0;
+		int count1 = 0;
 		if (checkString(s) == true) {
-			while (count < s.length()) {
+			while (count+1 < s.length()) {
+				
 				s1 = s.substring(count, count + 1);
 				s2 = s.substring(count + 1, count + 2);
 				if (s1.equals(s2)) {
-					count = count + 2;
+					count1++;
+					count++;
 					if (s3 != "") {
 						break;
 					}
 				} else {
-					count++;
-					s3 = s3.concat(s1);
+					if (count1 != 0) {
+						count1=0;
+						count++;
+					} else if(count1==0){
+						count++;
+						if(count==s.length()-1){
+						s3 = s3.concat(s1);
+						s3 = s3.concat(s2);
+						}else{
+						s3 = s3.concat(s1);
+						}
+					}
 				}
 			}
 			System.out.println(s3);
